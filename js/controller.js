6db53a70227557789ef79faf9875740bb6645e9e
Richard Schneider <richard@schneiderbox.net>
2014-07-02 18:09:00 -0500

    restructure project, breaking major constructors into different files and moving all JS files into the js/ directory

---
+++ js/controller.js
@@ -0,0 +1,81 @@
+'use strict';
+
+function Controller(camera){
+	this.camera = camera;
+	this.rotateDir = 0;
+
+	this.states = {
+		left: false,
+		right: false,
+		up: false,
+		down: false
+	}
+	
+	var controller = this;
+	document.addEventListener('keydown', function(event){
+		switch(event.keyCode){
+			case 39:
+			case 68:
+				controller.states.right = true
+				break;
+			case 37:
+				controller.states.left = true
+				break;
+			case 38:
+			case 87:
+				controller.states.up = true;
+				break;
+			case 40:
+			case 83:
+				controller.states.down = true;
+				break;
+		}
+	});
+	document.addEventListener('keyup', function(event){
+		switch(event.keyCode){
+			case 39:
+			case 68:
+				controller.states.right = false;
+				break;
+			case 37:
+				controller.states.left = false;
+				break;
+			case 38:
+			case 87:
+				controller.states.up = false;
+				break;
+			case 40:
+			case 83:
+				controller.states.down = false;
+				break;
+		}
+	});
+}
+Controller.prototype.update = function(delta){
+	var ROTATE_SPEED = 2*Math.PI/3/1000;
+	var MOVE_SPEED = 7/1000;
+	var camera = this.camera;
+	
+	if(this.states.right){
+		camera.rotate(-ROTATE_SPEED * delta);
+	}
+	if(this.states.left){
+		camera.rotate(ROTATE_SPEED * delta);
+	}
+	if(this.states.up){
+		if(map[Math.floor(camera.pos.x + camera.dir.x*MOVE_SPEED*delta)][Math.floor(camera.pos.y)] == 0){
+			camera.pos.x += camera.dir.x * MOVE_SPEED * delta;
+		}
+		if(map[Math.floor(camera.pos.x)][Math.floor(camera.pos.y + camera.dir.y*MOVE_SPEED*delta)] == 0){
+			camera.pos.y += camera.dir.y * MOVE_SPEED * delta;
+		}
+	}
+	if(this.states.down){
+		if(map[Math.floor(camera.pos.x - camera.dir.x*MOVE_SPEED)][Math.floor(camera.pos.y)] == 0){
+			camera.pos.x -= camera.dir.x * MOVE_SPEED * delta;
+		}
+		if(map[Math.floor(camera.pos.x)][Math.floor(camera.pos.y - camera.dir.y*MOVE_SPEED)] == 0){
+			camera.pos.y -= camera.dir.y * MOVE_SPEED * delta;
+		}
+	}
+}
---
+++ js/game.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function Game(canvas){
+	this.canvas = canvas;
+	this.ctx = canvas.getContext('2d');
+	this.lastTime = 0;
+}
+Game.prototype.start = function(){
+	var gameloop = this;
+
+	var camera = new Camera();
+	window.camera = camera;
+
+	var controller = new Controller(camera);
+
+	function frame(time){
+		var delta = time - gameloop.lastTime;
+		gameloop.lastTime = time;
+
+		camera.update(delta);
+		controller.update(delta);
+
+		camera.render(gameloop.canvas, gameloop.ctx);
+
+		window.requestAnimationFrame(frame);
+	}
+	window.requestAnimationFrame(frame);
+}
+
+var game = new Game(document.getElementById('canvas'));
+game.start();
